474aa2c63d22cfe7d31a43565798320ffea540bf
Maciej Frydrychowicz <maciej.frydrychowicz@artifexmundi.com>
2017-09-11 11:45:36 +0200

    [Game] Introduce TriangleGravityCenterExcercise.

--- Assets/GameModule/Scripts/Excercises/TriangleGravityCenterExcercise.cs
+++ Assets/GameModule/Scripts/Excercises/TriangleGravityCenterExcercise.cs
@@ -15,17 +15,31 @@ namespace Assets.GameModule.Scripts.Excercises {
         }
 
         public override Vector2[] GetUser(Vector2[] excercisePoints) {
-            Vector2[] result = new Vector2[2];
+            Vector2[] result = new Vector2[5];
             result[0] = excercisePoints[0];
             result[1] = new Vector2(Random.Range(0, 1f) * size, Random.Range(0, 1f) * size);
+            result[2] = excercisePoints[1];
+            result[3] = result[1];
+            result[4] = excercisePoints[2];
             return result;
         }
 
+        public override void UpdateUser(Vector2[] points, Vector2 move) {
+            points[1] = points[3] += move;
+        }
+
         public override float Validate(Vector2[] excercisePoints, Vector2[] userPoints) {
+            float edgeSum =
+                Mathf.Sqrt(Mathf.Pow(excercisePoints[1].x - excercisePoints[0].x, 2) + Mathf.Pow(excercisePoints[1].y - excercisePoints[0].y, 2)) +
+                Mathf.Sqrt(Mathf.Pow(excercisePoints[2].x - excercisePoints[1].x, 2) + Mathf.Pow(excercisePoints[2].y - excercisePoints[1].y, 2)) +
+                Mathf.Sqrt(Mathf.Pow(excercisePoints[0].x - excercisePoints[2].x, 2) + Mathf.Pow(excercisePoints[0].y - excercisePoints[2].y, 2));
+
             Vector2 center = new Vector2(
                 (excercisePoints[0].x + excercisePoints[1].x + excercisePoints[2].x) / 3,
                 (excercisePoints[0].y + excercisePoints[1].y + excercisePoints[2].y) / 3);
-            return Mathf.Sqrt(Mathf.Pow(center.x - userPoints[1].x, 2) + Mathf.Pow(center.y - userPoints[1].y, 2));
+            float error = Mathf.Sqrt(Mathf.Pow(center.x - userPoints[1].x, 2) + Mathf.Pow(center.y - userPoints[1].y, 2));
+
+            return (error * 100) / edgeSum;
         }
     }
 }
